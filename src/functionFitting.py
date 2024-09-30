080bb84fe91379175c8be6c99949c90a405cf337
EvanGibsonSmith <114107230+EvanGibsonSmith@users.noreply.github.com>
2024-09-30 10:57:27 -0400

    added xor/another sample function

--- src/functionFitting.py
+++ src/functionFitting.py
@@ -11,8 +11,17 @@ def funcDampenedBubbleWrap(x, y):
     numer = (x**2) + (y**2) + 1
     return denom / numer
 
+def sampleFunc(x, y):
+    return (np.sin(x*y) - np.cos(y)) / ((x**2 * y**2) + 1)
+
+def xor(x, y):
+    x = np.where(x>0, 1, 0)
+    y = np.where(y>0, 1, 0)
+    xor = np.logical_xor(x, y)
+    return np.where(xor==False, 0, 1)
+
 X, Y = np.meshgrid(x_range, y_range)
-Z = funcDampenedBubbleWrap(X, Y)
+Z = sampleFunc(X, Y)
 
 inputs = np.vstack((X.ravel(), Y.ravel()))
 outputs = np.atleast_2d(Z.ravel())
@@ -27,18 +36,18 @@ nnWide.setupTraining(inputs, outputs, learningRate)
 #nn.train(inputs, outputs, learning_rate=[learningRate])
 
 # Train each epoch manually to see progres
-numEpochs = 10
-graphEpochRate = 2
+numEpochs = 100
+graphEpochRate = 20
 for epoch in range(numEpochs):
-    nnDeep.run_epoch(trainX, trainY, batch_size=(trainX.shape[1])//50)
+    nnDeep.run_epoch(trainX, trainY, batch_size=(trainX.shape[1])//50, alpha_momentum=0.4)
     nnWide.run_epoch(trainX, trainY, batch_size=(trainX.shape[1])//50)
-    if (epoch%graphEpochRate==0):
+    if ((epoch%graphEpochRate==0 or epoch==0) and graphEpochRate!=-1):
         # Plot underlying function and neural network output
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.plot_wireframe(X, Y, Z, color='red', linewidth=0.5, rstride=6, cstride=6) 
         Z_deep_nn = nnDeep.get_yhat(inputs)[0].reshape(x_num, y_num)
         ax.plot_wireframe(X, Y, Z_deep_nn, color="blue", linewidth=0.5, rstride=3, cstride=3) 
-        Z_wide_nn = nnWide.get_yhat(inputs)[0].reshape(x_num, y_num)
-        ax.plot_wireframe(X, Y, Z_wide_nn, color="green", linewidth=0.5, rstride=3, cstride=3) 
+        #Z_wide_nn = nnWide.get_yhat(inputs)[0].reshape(x_num, y_num)
+        #ax.plot_wireframe(X, Y, Z_wide_nn, color="green", linewidth=0.5, rstride=3, cstride=3) 
         plt.show()
